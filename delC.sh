1af1380090f6ce1d9e8662285b6a86030890aab3
wolfwithcode <wolfwithcode@gmail.com>
2019-10-01 21:33:37 +1000

    add new function

--- delC.sh
+++ delC.sh
@@ -1,3 +1,18 @@
+function deleteAFile()
+{
+  file=$1
+  #echo "delete file $file"
+  echo "Displaying first 10 lines of $file:"
+  echo $(cat $file | head -10)
+  echo -e "Delete file $file? (y/n): \c"
+  read answer
+  case $answer in
+       Y|y) echo -e "File $file deleted.";;
+       N|n) echo -e "File $file NOT deleted.";;
+         *) echo "Invalid option";;
+  esac
+  return
+}
 numberOfArguments=$#
 echo "the number of arguments: $numberOfArguments"
 if [ $numberOfArguments -eq 0 ]; then
@@ -14,18 +29,20 @@ if [ $numberOfArguments -eq 0 ]; then
   inputUser=n
   for file in "${listOfFileC[@]}"
   do
-    echo "Displaying first 10 lines of $file:"
-    echo $(cat $file | head -10)
-    echo -e "Delete file $file? (y/n): \c"
-    read answer
-    #echo $answer
-    case $answer in
-         Y|y) echo -e "File $file deleted.";;
-         N|n) echo -e "File $file NOT deleted.";;
-           *) echo "Invalid option";;
-    esac
+    deleteAFile $file
   done
  fi
 else
- echo "have arguments"
+  echo "have arguments"
+  echo "the complete set of arguments is: $*"
+  listOfFileC=("$@")
+  for file in "${listOfFileC[@]}"
+  do
+    if [ -f $file ] ; then
+        echo "File $file exists"
+        deleteAFile $file
+    else
+        echo "File $file does not exist."
+    fi
+  done
 fi
